fa6cb2f781fc9ecb38ea6e85b8c181b0a422ae0c
DeaDvey <maxcrawshaw3@hotmail.com>
2023-12-12 08:10:33 +0000

    added blog home page

--- template.js
+++ template.js
@@ -4,21 +4,24 @@ let divText = div.innerHTML;
 console.log(divText);
 
 let newPageHTML = `
+<!DOCTYPE html>
+<html>
     <head>
         <meta charset="UTF-8"/>
         <title>DeaDvey's Domain</title>
-        <icon src="deadvey-icon-circle.ico"></icon>
-        <link rel="stylesheet" type="text/css" href="index.css">
+        <link rel="icon" href="../favicon.png">
+        <link rel="stylesheet" type="text/css" href="http://deadvey.com/index.css">
     </head>
     <body>
         <div class="header main">
-            <img class="header-image" /><br/><a href="https://deadvey.xyz">Home page</a> <a href="../blog">Blogs</a> <a href="../videos">Videos</a><hr/>
+            <img class="header-image" /><br/><a href="http://deadvey.com">Home page</a> <a href="blog/index.html">Blogs</a> <a href="videos/index.html">Videos</a><hr/>
         </div>
         <div class="main">
             ${divText}
         </div>
         <div class="footer">
         </div>
-    </body>`
+    </body>
+</html>`
 
 document.body.innerHTML = newPageHTML
