09160485f20f8ec6c5814cba5b2cb2e7afebf3fa
DeaDvey <maxcrawshaw3@hotmail.com>
2023-12-12 18:00:21 +0000

    added a footer and made the blog template stuff better TODO: Make the template more centralised as I have 3

--- blog/blogs.js
+++ blog/blogs.js
@@ -1,3 +1,4 @@
-let blogs = [["Test post2", "Testicy Test"],
-["Test post", "Testy test!"]]
+let blogs = [["I didn't like Wordpress","So... Yeah, I didn't like wordpress very much, it's pretty clunky and I don't have much control over the webpage, and it's kinda not that fun to use, I'm glad I tried it out though because I kind of know how to use it know I suppose, I might copy over the wordpress back over to this so I can use it myself for fun I suppose."],
+    ["Test post2", "Testicy Test"],
+    ["Test post", "Testy test!"]]
 
