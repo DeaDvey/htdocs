5ebc3162dc69db2b7a2d6570debd08505c949c7e
DeaDvey <maxcrawshaw3@hotmail.com>
2023-12-24 17:05:22 +0000

    added a html.js which defines the header and footer of every html page

--- blog/template.js
+++ blog/template.js
@@ -7,91 +7,13 @@ let divText = div.innerHTML;
 
 console.log(divText);
 
-let newPageHTML = `
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8"/>
-        <title>DeaDvey's Domain</title>
-        <link rel="icon" href="../favicon.png">
-        <link rel="stylesheet" type="text/css" href="../index.css">
-    </head>
-    <body>
-        <div class="header main">
-              <img class="header-image" /><br/><a href="http://deadvey.com">Home page</a> | <a href="index.html">Blogs</a> <a href="feed.xml"><img width="16" src="../feedIcon.png" /></a> | <a href="../videos/index.html">Videos</a>
-
-        </div>
-        <hr/>
-        <div class="main">
-            ${divText}
-            <div id="links">
-            </div>
-        </div>
-        <div class="footer">
-            <hr/>
-            <li>
-                Internal:
-                <li><a href="theunderlandcouk" >theunderland webpages (internal link)</a></li>
-                <li><a href="experiments.html" >Experiments (internal link)</a></li>
-                <br/>
-                External:
-                <li><a href="https://github.com/DeaDvey" >Github (external link)</a></li>
-                <li><a href="https://gitlab.com/deadvey" >Gitlab (external link)</a></li>
-                <li><a href="https://mastodon.social/@DeaDvey" >Mastadon.social (external link)</a></li>
-            </li>
-        </div>
-    </body>
-</html>`
-
+let newPageHTML = `${pageTop}${divText}${pageBottom}`
 document.body.innerHTML = newPageHTML  //apply the template
 
 ///////////////////////////
 //     ADD THE LINKS
 ///////////////////////////
 
-function loadContent(page, content, header, date) {
-    console.log(page)
-    let blogText = content
-    console.log(blogText)
-    let newPageHTML = `
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="UTF-8"/>
-            <title>DeaDvey's Domain</title>
-            <link rel="icon" href="../favicon.png">
-            <link rel="stylesheet" type="text/css" href="../index.css">
-        </head>
-        <body>
-            <div class="header main">
-                    <img class="header-image" /><br/><a href="http://deadvey.com">Home page</a> | <a href="index.html">Blogs</a> <a href="feed.xml"><img width="16" src="../feedIcon.png" /></a> | <a href="../videos/index.html">Videos</a>
-
-            </div>
-            <hr/>
-            <div class="main">
-                <h3 class="blog-date">${date}</h3>
-                <h1 class="blog-title">${header}</h1>
-                <p class="blog-content">${content}</p>
-                <div id="links">
-                </div>
-            </div>
-            <div class="footer">
-                <hr/>
-                <li>
-                    Internal:
-                    <li><a href="theunderlandcouk" >theunderland webpages (internal link)</a></li>
-                    <li><a href="experiments.html" >Experiments (internal link)</a></li>
-                    <br/>
-                    External:
-                    <li><a href="https://github.com/DeaDvey" >Github (external link)</a></li>
-                    <li><a href="https://gitlab.com/deadvey" >Gitlab (external link)</a></li>
-                    <li><a href="https://mastodon.social/@DeaDvey" >Mastadon.social (external link)</a></li>
-                </li>
-            </div>
-        </body>
-    </html>`
-    document.body.innerHTML = newPageHTML
-}
 
 function escapeQuotes(value) {
     return value.replace(/["'&<>]/g, function (char) {
@@ -116,7 +38,6 @@ let linksDiv = document.getElementById("links")
 
 let linksText = "<li><a href='blogs/all.html'>ALL POSTS</a></li><br/>"
 
-let length = blogs.length
 
 for (let page = blogs.length - 1; page >= 0; page--) {
     console.log(length)
--- template.js
+++ template.js
@@ -3,37 +3,6 @@ let divText = div.innerHTML;
 
 console.log(divText);
 
-let newPageHTML = `
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8"/>
-        <title>DeaDvey's Domain</title>
-        <link rel="icon" href="../favicon.png">
-        <link rel="stylesheet" type="text/css" href="index.css">
-    </head>
-    <body>
-        <div class="header main">
-            <img class="header-image" /><br/><a href="http://deadvey.com">Home page</a> | <a href="blog/index.html">Blogs</a> <a href="blog/feed.xml"><img width="16" src="feedIcon.png" /></a> | <a href="videos/index.html">Videos</a>
-        </div>
-        <hr/>
-        <div class="main">
-            ${divText}
-        </div>
-        <div class="footer">
-            <hr/>
-            <li>
-                Internal:
-                <li><a href="theunderlandcouk" >theunderland webpages (internal link)</a></li>
-                <li><a href="experiments.html" >Experiments (internal link)</a></li>
-                <br/>
-                External:
-                <li><a href="https://github.com/DeaDvey" >Github (external link)</a></li>
-                <li><a href="https://gitlab.com/deadvey" >Gitlab (external link)</a></li>
-                <li><a href="https://mastodon.social/@DeaDvey" >Mastadon.social (external link)</a></li>
-            </li>
-        </div>
-    </body>
-</html>`
+let newPageHTML = `${pageTop}${divText}${pageBottom}`
 
 document.body.innerHTML = newPageHTML
