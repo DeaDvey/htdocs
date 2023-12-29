505fa3f86c52939d201eb94ca52667299636bf95
DeaDvey <maxcrawshaw3@hotmail.com>
2023-12-29 01:35:11 +0000

    added link to nodejs repo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # htdocs
 Files for my website (deadvey.com)
+
+See https://github.com/DeaDvey/node for the nodeJS files
--- video/videos.js
+++ video/videos.js
@@ -1,4 +1,4 @@
-let videos = [["Linus drops the hard R.mp4","2023-12-21T20:00:56.000Z"],["aoh2-cherokee.mp4","2023-12-20T08:16:43.000Z"],["conquest-of-america.mp4","2023-12-19T20:34:29.000Z"],["iwbtg1.mp4","2023-09-21T19:28:42.000Z"],["covid.mp4","2023-08-12T17:13:02.000Z"],["Creeper.webm","2023-08-12T17:13:00.000Z"],["piston.webm","2023-08-12T17:08:27.000Z"],["coloride.webm","2023-08-12T16:14:03.000Z"]]
+let videos = [["sus.txt","2023-12-29T01:30:39.362Z"],["Linus drops the hard R.mp4","2023-12-21T20:00:56.000Z"],["aoh2-cherokee.mp4","2023-12-20T08:16:43.000Z"],["conquest-of-america.mp4","2023-12-19T20:34:29.000Z"],["iwbtg1.mp4","2023-09-21T19:28:42.000Z"],["covid.mp4","2023-08-12T17:13:02.000Z"],["Creeper.webm","2023-08-12T17:13:00.000Z"],["piston.webm","2023-08-12T17:08:27.000Z"],["coloride.webm","2023-08-12T16:14:03.000Z"]]
 
 if (typeof module !== 'undefined' && module.exports) {
 	module.exports = videos;
