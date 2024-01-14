018794eaee633544f538a453da6cdc66b3b23f10
DeaDvey <maxcrawshaw3@hotmail.com>
2024-01-14 00:09:31 +0000

    before sunday

---
+++ blog/submit-comment.js
@@ -0,0 +1,17 @@
+let commentForm = document.getElementById("commentForm");
+
+
+commentForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  
+  let name = document.getElementById("name")
+  let comment = document.getElementById("comment")
+
+  console.log(name.value)
+  console.log(comment.value)
+  
+  let post = name.name // The post number ie 0 is stored in the name tag of the input field
+
+  comments[post].push(`${name.value}:  ${comment.value}`)
+  console.log(comments)
+});
