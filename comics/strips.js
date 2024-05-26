f6d2f07609a55fcaed7973400cb67589347f7ab2
DeaDvey <maxcrawshaw3@hotmail.com>
2024-05-26 16:36:21 +0100

    update

--- comics/strips.js
+++ comics/strips.js
@@ -1,4 +1,4 @@
-let strips = [["RevisionBreak.png","2024-05-26T15:24:38.240Z"],["macbeth-macbeth-where-fore-art-thou-macbeth.png","2024-05-26T15:24:38.240Z"]]
+let strips = [["RevisionBreak.png","2024-05-26T15:31:10.091Z"],["macbeth-macbeth-where-fore-art-thou-macbeth.png","2024-05-26T15:31:10.091Z"]]
 strips = strips.reverse()
 if (typeof module !== 'undefined' && module.exports) {
        module.exports = strips;
