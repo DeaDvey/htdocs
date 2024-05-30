0a94708fadf322bf7a16d3b0fbce866dfb380a54
DeaDvey <maxcrawshaw3@hotmail.com>
2024-05-30 14:49:14 +0100

    added blog

--- blog/comments.js
+++ blog/comments.js
@@ -1,4 +1,4 @@
-comments=[[["Man","Do I love apple Juice!"]],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[["DeaDVey","HEY GUYS!"]],[["sussy","baka"],["max","Hello guys!"],["not_deadvey","I totally agree, and DeaDvey is SO cool and awesome btw!!!"],["deadvey","Aww, thanks so much not_deadvey!"],["not_deadvey","OMFG A CELEBRITY SPOKE TO ME!!!!!!!! :O"]],[],[],[],[],[],[],[],[],[],[],[]]
+comments=[[["Man","Do I love apple Juice!"],["test1","test"],["test2","TEST! :O <b>hello</b>"],["test","console.log(\"alert\")"],["","test"],["etst",""]],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[["DeaDVey","HEY GUYS!"]],[["sussy","baka"],["max","Hello guys!"],["not_deadvey","I totally agree, and DeaDvey is SO cool and awesome btw!!!"],["deadvey","Aww, thanks so much not_deadvey!"],["not_deadvey","OMFG A CELEBRITY SPOKE TO ME!!!!!!!! :O"]],[["deadvey","ok, here's the comments"],["deadvey","From w3m!"]],[],[],[],[],[],[],[],[],[],[]]
 if (typeof module !== "undefined" && module.exports) {
-                module.exports = comments;                 
+                module.exports = comments;     
 }
