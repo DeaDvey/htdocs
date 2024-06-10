1e152c626bbed941da54a69fe60fa01c3f0f862d
DeaDvey <maxcrawshaw3@hotmail.com>
2024-06-10 17:26:34 +0100

    reviewed werid

--- html.js
+++ html.js
@@ -5,6 +5,7 @@ let pageTop = `<!DOCTYPE html>
         <title>DeaDvey's Domain</title>
         <link rel="icon" href="/images/favicon.png">
         <link rel="stylesheet" type="text/css" href="/index.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     </head>
     <body>
         <div class="header main">
